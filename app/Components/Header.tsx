a6009bc1b6f7756f54fbb4f7855351001e152dd1
manjunathjk2406 <manjunathkavalikai9@gmail.com>
2025-10-11 14:16:56 +0530

    "new update"

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
               initial = {{y:30,opacity:0}} 
               whileInView = {{y:0,opacity:1}} 
               transition={{duration:0.6, delay:1}}  
-              href="./myresume.docx" download className='px-5 py-1 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume
+              href="./manjunathk-resume.docx" download className='px-5 py-1 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume
                 <Image alt="" src={assets.download_icon} className='w-4'/>
               </motion.a>
             </div>
